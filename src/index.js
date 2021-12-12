020345853a6fe49e71154d20603022ae738c11eb
Antonio <antoniomaestregarrido.com>
2021-12-12 05:54:27 +0100

    getting data from server

--- src/index.js
+++ src/index.js
@@ -3,15 +3,44 @@ const APIKEY="94bb022db166cb23ddcccab435aa0727"
             
 var city="Malaga";
 const root=document.querySelector("#root")
+
+
+
 getData()
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}  "https://api.giphy.com/v1/gifs/translate?api_key=2Tds4gFBxYcb2yflSus1WvEd20FGYuMf&s="
+
+
+function createObjectData(rawData) {
+    //var rawData= getData();
+    console.log(rawData)
+    console.log(rawData.name)
+    var data={
+        cityname:rawData.name,
+        weather:rawData.weather.description,
+        temp:rawData.main.temp,
+        feelLike:rawData.main.feels_like,
+        wind:rawData.wind.speed
+
+
+
+
+    }
+    console.table(data)
+    
+}
+
+
+
+
 async function getData() {
+    
     const getclima= await fetch("https://api.openweathermap.org/data/2.5/weather?q=cordoba&appid=94bb022db166cb23ddcccab435aa0727",{mode:"cors"})
-    //const getclima= await fetch("https://api.giphy.com/v1/gifs/translate?api_key=2Tds4gFBxYcb2yflSus1WvEd20FGYuMf&s=",{mode:"cors"})
+    
     rawData= await getclima.json()
-    console.table(rawData)
-    console.log(typeof(rawData))
-    root.textContent=rawData.name
+    console.log(typeof("el tipo es "+rawData))
+    createObjectData(rawData)
+    return rawData
+  
 
 
     
